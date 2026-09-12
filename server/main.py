4f8ab23cb2588a4b467d1a2c5be297f65c357bdb
Raleese <titaslr38@gmail.com>
2026-09-12 15:03:42 +0300

    Added get for metrics

--- server/main.py
+++ server/main.py
@@ -12,3 +12,13 @@ def health():
 def metrics(data: dict):
     db.insert_metrics(data)
     return {"status": "metrics received"}
+
+@app.get("/metrics/latest")
+def metrics_latest():
+    rows = db.get_latest_metrics()
+    return rows
+
+@app.get("/metrics/history")
+def metrics_history():
+    rows = db.get_all_metrics()
+    return rows
