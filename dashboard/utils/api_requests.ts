ca568c183a7d9fe5992ad5e2dc4d2e0053789dc7
Titas <titaslr38@gmail.com>
2026-09-17 02:14:45 +0300

    Added API requests for getting device ID's

--- dashboard/utils/api_requests.ts
+++ dashboard/utils/api_requests.ts
@@ -1,9 +1,9 @@
-import type { Metrics, Alert } from './interfaces';
+import type { Metrics, Alert, Device } from './interfaces';
 
-const API_URL = "http://localhost:8000";
+const API_URL = "http://192.168.0.4:8000";
 
-export async function getLatestMetrics(): Promise<Metrics>{
-    const response = await fetch(`${API_URL}/metrics/latest`);
+export async function getLatestMetrics(device_id: string): Promise<Metrics>{
+    const response = await fetch(`${API_URL}/metrics/latest?device_id=${device_id}`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch latest metrics");
@@ -12,8 +12,8 @@ export async function getLatestMetrics(): Promise<Metrics>{
     return response.json();
 }
 
-export async function getMetricsHistory(): Promise<Metrics[]> {
-    const response = await fetch(`${API_URL}/metrics/history`);
+export async function getMetricsHistory(device_id: string): Promise<Metrics[]> {
+    const response = await fetch(`${API_URL}/metrics/history?device_id=${device_id}`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch metrics history");
@@ -22,11 +22,21 @@ export async function getMetricsHistory(): Promise<Metrics[]> {
     return response.json(); 
 }
 
-export async function getAlerts(): Promise<Alert[]> {
-    const response = await fetch(`${API_URL}/metrics/alerts`);
+export async function getAlerts(device_id: string): Promise<Alert[]> {
+    const response = await fetch(`${API_URL}/metrics/alerts?device_id=${device_id}`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch alerts");
+    }
+
+    return response.json();
+}
+
+export async function getDeviceIds(): Promise<Device[]> {
+    const response = await fetch(`${API_URL}/metrics/devices`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch device IDs");
     }
 
     return response.json();
