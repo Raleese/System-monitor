f772450e4e03184e2ba47fb9b885d85ad624b3ba
Titas <titaslr38@gmail.com>
2026-09-17 02:15:15 +0300

    Updated methods for multiple devices

--- server/database.py
+++ server/database.py
@@ -10,65 +10,92 @@ def get_db_connection():
 def create_metrics_table():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('''
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS metrics (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            device_id TEXT NOT NULL,
             hostname TEXT NOT NULL,
             cpu REAL NOT NULL,
             memory REAL NOT NULL,
             disk REAL NOT NULL,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         )
-    ''')
+    """)
     conn.commit()
     conn.close()
 
 def insert_metrics(data):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO metrics (hostname, cpu, memory, disk)
-        VALUES (?, ?, ?, ?)
-    ''', (data["hostname"], data["cpu"], data["memory"], data["disk"]))
+    cursor.execute("""
+        INSERT INTO metrics (device_id, hostname, cpu, memory, disk)
+        VALUES (?, ?, ?, ?, ?)
+    """, (data["device_id"], data["hostname"], data["cpu"], data["memory"], data["disk"]))
     conn.commit()
     conn.close()
 
     delete_old_metrics(keep_count=1000)  # Keep only the latest 1000 records
 
-def get_all_metrics():
+def get_all_metrics(device_id : str):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('''SELECT * FROM metrics ORDER BY id ASC''')
+    cursor.execute("""
+        SELECT * 
+        FROM metrics 
+        WHERE device_id = ?
+        ORDER BY id ASC
+    """, (device_id,))
     rows = cursor.fetchall()
     conn.close()
     return rows
 
-def get_latest_metrics():
+def get_latest_metrics(device_id : str):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('''
+    cursor.execute("""
             SELECT * 
             FROM metrics
+            WHERE device_id = ?
             ORDER BY id DESC
             LIMIT 1
-        ''')
+        """, (device_id,))
     row = cursor.fetchone()
     conn.close()
     return row
+
+def get_device_ids():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT device_id, hostname
+        FROM metrics
+        GROUP BY device_id, hostname
+        ORDER BY hostname ASC
+    """)
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
 def delete_old_metrics(keep_count: int = 1000):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("""
-        DELETE FROM metrics
-        WHERE id NOT IN (
-            SELECT id
-            FROM metrics
-            ORDER BY id DESC
-            LIMIT ?
-        )
-    """, (keep_count,))
+    rows = getdevice_ids()
+
+    for row in rows:
+        device_id = row[0]
+
+        cursor.execute("""
+            DELETE FROM metrics
+            WHERE devuce_id = ?
+            AND id NOT IN (
+                SELECT id
+                FROM metrics
+                WHERE device_id = ?
+                ORDER BY id DESC
+                LIMIT ?
+            )
+        """, (device_id, keep_count,))
 
     conn.commit()
     cursor.execute("VACUUM")
